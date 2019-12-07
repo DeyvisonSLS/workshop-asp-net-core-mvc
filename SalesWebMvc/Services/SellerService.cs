4aae5bee7423957d425afebac46e3e3c2f32053b
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-12-07 12:33:53 -0300

    Foreign key not null (referencial integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -18,5 +18,11 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj)
+        {
+            obj.Department = _context.Department.First();
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }

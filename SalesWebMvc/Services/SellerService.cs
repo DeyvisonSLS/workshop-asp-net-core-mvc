30fcc2fc72798357ace0f5a9af0fe0ab4f4b101b
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-12-09 17:38:25 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -37,7 +37,7 @@ namespace SalesWebMvc.Services
                 _context.Seller.Remove(await FindByIdAsync(id));
                 await _context.SaveChangesAsync();
             }
-            catch(DbUpdateException e)
+            catch(DbUpdateException)
             {
                 throw new IntegrityException("Can't delete seller because he/she has sales.");
             }
